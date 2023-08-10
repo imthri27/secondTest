ff5dc3b5eaa7a812482b364b429ab7e727cb4b74
imthri27 <thuyyoon2710@gmail.com>
2023-08-10 09:20:07 +0700

    first-commit

---
+++ j1.js
@@ -0,0 +1,100 @@
+// var data = [
+//   {
+//     birthday: "2023-05-28T17:40:23.974Z",
+//     name: "Marlene Trantow",
+//     address: "08352",
+//     phoneNumber: "795.889.7185",
+//     gender: true,
+//     id: "1",
+//   },
+//   {
+//     birthday: "2023-05-29T01:32:28.094Z",
+//     name: "Charlotte Willms PhD",
+//     address: "527",
+//     phoneNumber: "1-674-509-4281",
+//     gender: true,
+//     id: "2",
+//   },
+//   {
+//     birthday: "2023-05-28T10:08:23.173Z",
+//     name: "Al Abernathy Sr.",
+//     address: "2506",
+//     phoneNumber: "635-220-9829 x7066",
+//     gender: true,
+//     id: "3",
+//   },
+//   {
+//     birthday: "2023-05-28T23:39:32.113Z",
+//     name: "Mr. Danny Beer",
+//     address: "5713",
+//     phoneNumber: "1-759-247-5965 x7435",
+//     gender: true,
+//     id: "4",
+//   },
+//   {
+//     birthday: "2023-05-28T07:06:55.659Z",
+//     name: "Douglas Friesen",
+//     address: "5323",
+//     phoneNumber: "787-716-6200 x84833",
+//     gender: false,
+//     id: "5",
+//   },
+//   {
+//     birthday: "2023-05-28T09:01:22.250Z",
+//     name: "Derek Konopelski",
+//     address: "7484",
+//     phoneNumber: "548-426-8396 x753",
+//     gender: false,
+//     id: "6",
+//   },
+//   {
+//     birthday: "2023-05-28T06:38:39.318Z",
+//     name: "Dr. Melanie Frami",
+//     address: "3501",
+//     phoneNumber: "1-970-656-3518",
+//     gender: false,
+//     id: "7",
+//   },
+//   {
+//     birthday: "2023-05-28T10:39:47.988Z",
+//     name: "Homer Lemke V",
+//     address: "62617",
+//     phoneNumber: "1-268-477-7594 x22729",
+//     gender: true,
+//     id: "8",
+//   },
+//   {
+//     birthday: "2023-05-28T02:21:06.381Z",
+//     name: "Stacey Wintheiser",
+//     address: "206",
+//     phoneNumber: "558.813.2124 x5305",
+//     gender: true,
+//     id: "9",
+//   },
+//   {
+//     birthday: "2023-05-28T06:14:53.320Z",
+//     name: "Dr. Marcella Leannon",
+//     address: "6703",
+//     phoneNumber: "289.222.7989",
+//     gender: false,
+//     id: "10",
+//   },
+// ];
+//  function sum(a,b){
+//     return a>b;
+//  }
+//  var sum = function(a,b){
+//      return a+b;
+     
+//     }
+//     console.log(sum(1,2));
+
+
+// function run(object){
+//     for(var key in object)
+//     console.log(object[key]);
+// }
+// console.log(run({ name: 'Nguyen Van A', age: 16 }));
+
+var array = [1,2,3,1,2,3];
+console.log([...new Set(array)]);
